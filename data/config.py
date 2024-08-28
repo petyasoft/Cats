be26aea0d2fdb7e4336d5788cec4b49c61e370d0
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-08-28 14:25:24 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,37 @@
+# api id, hash
+API_ID = 11
+API_HASH = 'hash'
+
+USE_TG_BOT = False
+BOT_TOKEN = '' # your bot token from BotFather
+CHAT_ID = '' # your user id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [1, 50] #[min,max] seconds
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# реф код
+REF_CODE = ''
+
+# использовать прокси
+USE_PROXY = True # True/False
+
+# задержка в секундах между выполнеными тасками
+TASK_SLEEP = [20, 40] #[min,max] 
+
+# мини задержки
+MINI_SLEEP = [1,20]
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
